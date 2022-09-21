4a7d673c2104a6736fc362941d5f5570a7f2c4b8
shongq <shq0716@gmail.com>
2022-09-21 17:09:59 +0900

    [api] updateKeywordSearchCount - CacheEvict 추가

--- blog-api/src/main/java/com/assignment/blog/service/KeywordService.java
+++ blog-api/src/main/java/com/assignment/blog/service/KeywordService.java
@@ -5,6 +5,7 @@ import com.assignment.blog.domain.Keyword;
 import com.assignment.blog.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,6 +37,7 @@ public class KeywordService {
      *
      * @param word
      */
+    @CacheEvict(value = "findTop10Keywords", allEntries = true)
     @Transactional
     public void updateKeywordSearchCount(String word) {
 //        while(!localLockProvider.tryAcquireLock(word)){};
